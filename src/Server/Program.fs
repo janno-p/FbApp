2be76270b91c3fad03dc7728d94c7c4c3d32b607
Janno Põldma <janno.poldma@gmail.com>
2018-05-29 11:38:33 +0300

    Build system.

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -1,8 +1,72 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿module FbApp.Server.Program
 
-open System
+open FbApp.Server.HttpsConfig
+open Giraffe
+open Giraffe.HttpStatusCodeHandlers
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Configuration.UserSecrets
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.FileProviders
+open Saturn
+open System.IO
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let clientPath = Path.Combine("..", "Client") |> Path.GetFullPath
+
+let apiRouter = scope {
+    post "/tokeninfo" (Successful.OK "")
+    post "/tokensignin" (Successful.OK "")
+    post "/tokensignout" (Successful.OK "")
+}
+
+let mainRouter = scope {
+    get "/" (Path.Combine(clientPath, "index.html") |> ResponseWriters.htmlFile)
+    forward "/api" apiRouter
+}
+
+let endpoints = [
+    { EndpointConfiguration.Default with
+        Port = Some 5000 }
+    { EndpointConfiguration.Default with 
+        Port = Some 5001
+        Scheme = Https
+        FilePath = Some (Path.Combine(__SOURCE_DIRECTORY__, "..", "FbApp.pfx")) }
+]
+
+let configureServices (context: WebHostBuilderContext) (services: IServiceCollection) =
+    ()
+
+let configureAppConfiguration (context: WebHostBuilderContext) (config: IConfigurationBuilder) =
+    config.AddJsonFile("appsettings.json", optional=false, reloadOnChange=true)
+          .AddJsonFile(sprintf "appsettings.%s.json" context.HostingEnvironment.EnvironmentName, optional=true, reloadOnChange=true)
+          .AddEnvironmentVariables()
+          .AddUserSecrets<EndpointConfiguration>()
+    |> ignore
+
+let app = application {
+    router mainRouter
+    memory_cache
+    use_gzip
+
+    app_config (fun app ->
+        app.UseStaticFiles(
+            new StaticFileOptions(
+                FileProvider = new PhysicalFileProvider(clientPath),
+                RequestPath = PathString.Empty
+            )
+        )
+    )
+
+    host_config (fun host ->
+        host.UseKestrel(fun o -> o.ConfigureEndpoints endpoints)
+            .ConfigureAppConfiguration(configureAppConfiguration)
+            .ConfigureServices(configureServices)
+    )
+}
+
+run app
+
+[<assembly: UserSecretsIdAttribute("d6072641-6e1a-4bbc-bbb6-d355f0e38db4")>]
+do()
