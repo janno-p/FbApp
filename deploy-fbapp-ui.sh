20509f57802bbbdc0ba6b59adfc880a2a3c0dfe3
Janno Põldma <janno.poldma@gmail.com>
2021-06-22 17:43:17 +0300

    Use correct dockerfile

--- deploy-fbapp-ui.sh
+++ deploy-fbapp-ui.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-docker build -t localhost:32000/fbapp-ui:registry .
+docker build -t localhost:32000/fbapp-ui:registry -f fbapp-ui/Dockerfile .
 docker push localhost:32000/fbapp-ui:registry
